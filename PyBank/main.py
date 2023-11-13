48ea4fc88990b6d11a66063ab8f89dd3f167a6a8
Erik Anthony <eanthony_98@yahoo.com>
2023-11-13 16:05:04 -0600

    1st draft

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,33 @@
 
+import os
+import csv
+
+csvpath = os.path.join("..","pybank","resources","budget_data.csv")
+
+# Open and Read csv
+with open(csvpath) as csv_file:
+
+    csvreader = csv.reader(csv_file, delimiter=",")
+
+    csvheader = next(csvreader)
+
+    print("Financial Analysis")
+    print("---------------------------------------")
+
+
+    month =0
+
+    month =(len(list(csvreader)))  
+
+    month=[0]
+
+
+
+
+    
+
+                           
+                           
+                          
+
+
