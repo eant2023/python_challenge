3b3b91b88336415159016721549b5e5ff271534b
Erik Anthony <eanthony_98@yahoo.com>
2023-11-16 15:05:02 -0600

    my recent changes

--- Pypoll/main1.py
+++ Pypoll/main1.py
@@ -1,16 +1,9 @@
 
-# for this project I utilized the assistance of the following Youtube video to assist
-# with my coding:https://www.youtube.com/watch?v=cdwlWjjwxlc&list=PLBNHcTwrlK2h-3_kPuZo597i3mMUOA0oe&index=2&t=8s
-
-
 import os
 
 import csv
 
-#Paths to files to load and output
-
 election_path = os.path.join("..", "resources", "election_data.csv")
-election_path_out=("..","python_challege","Pypoll","analysis.txt")
 
 #Initialize variables
 all_candidates = []
@@ -20,17 +13,16 @@ win_cand = ["",0]
 win_tot = 0
 vote = 0
 
-# Open csv files and dictionary
+
 with open(election_path,"r") as csvfile:
 
      csvreader = csv.DictReader(csvfile)
 
-# Vote counter     
+     
      for row in csvreader:
           
           vote = vote + 1
-
-#Gets candidates names
+          
           cand_nm = row ["Candidate"]
 
                 
@@ -43,7 +35,7 @@ with open(election_path,"r") as csvfile:
           else:
                vote_choice[cand_nm] = vote_choice[cand_nm] + 1
       
-#Determine winner of election
+
 if (vote > win_tot):
  win_cand [1] = vote_choice
  win_cand [0] = cand_nm
@@ -51,20 +43,12 @@ if (vote > win_tot):
 winner = sorted(vote_choice.items(),)
          
       
-#Prints results and writes to text file
-with open(analysis, "w") as txt_file:
-    
-    election_results = (
-    f"\n\nElections= Results\n"
-    f"--------------------------\n"
-    f"Total Votes:      {vote}\n"
-    f"---------------------------\n"
-)
-print(election_results)
 
 print("Election Results")     
 print("------------------------------")
 print(f"Total Votes: {vote}")
 print("------------------------------")
-
+print(f"{vote_choice}")
 print(f"Winner" + str(winner[1]))   
+
+
